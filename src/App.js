3725e5ece3351f79254fe274ac4e79ca3937e032
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-10-20 23:14:14 -0700

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import IngredientChecklist from './components/IngredientChecklist';
 
 function App() {
   return (
     <div className="App">
-       <IngredientChecklist />
+      <Switch>
+      <Route path='/' component={IngredientChecklist} />
+      </Switch>
     </div>
   );
 }
