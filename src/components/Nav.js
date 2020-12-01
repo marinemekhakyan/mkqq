486ca345be65fce817dd02b9f4dd1bd1bbb0e97f
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-11-30 18:44:08 -0800

    adding hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = () => (
-    <div className='nav'>
-        <div className='nav-items'>
-        <Link className='nav-item' to='/'>
+    <div className='navigation'>
+        <input type="checkbox" class="navigation__checkbox" id="nav-toggle"/>
+
+        <label for="nav-toggle" class="navigation__button">
+        <span class="navigation__icon">&nbsp;</span>
+        </label>
+
+        <div class="navigation__background">&nbsp;</div>
+
+
+        {/* <FontAwesomeIcon icon={faHamburger} className='navigation__hamburger'/> */}
+
+        <nav className='navigation__nav'>
+         <div className='navigation__nav-items'>
+        <Link className='navigation__nav-items--item' to='/'>
             HOME
         </Link>
-        <Link className='nav-item' to='/about'>
+        <Link className='navigation__nav-items--item' to='/about'>
             ABOUT
         </Link>
-        <Link className='nav-item' to='/dinners'>
+        <Link className='navigation__nav-items--item' to='/dinners'>
             DINNERS
         </Link>
-        <Link className='nav-item' to='/planner'>
-            PLANNER
+        <Link className='navigation__nav-items--item' to='/planner'>
+            PLANNER 
         </Link>
-        </div>
+        </div> 
+        </nav>
+
     </div>
 );
 
