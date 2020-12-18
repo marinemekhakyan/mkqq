0256d5957014b113b69dacb5f29426beac132deb
kcalumpong <kcalumpong@gmail.com>
2020-12-17 23:37:08 -0800

    added santa animation

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,13 +1,24 @@
 import React from 'react';
- 
+import ReactAudioPlayer from 'react-audio-player'; 
+import BellsMusic from '../images/bells.mp3'
+
 const LoadingPage = () => (
     <div>
-        <span className="crown-still-one"></span>
-        <span className="crown-still-two"></span>
-        <span className="crown-still-three"></span>
-        <span className="crown-one"></span>
-        <span className="crown-two"></span> 
-        <span className="crown-six"></span>
+        <span className="santa"></span>
+        <span className="cloudOne"></span>
+        <span className="cloudTwo"></span>
+        <span className="cloudThree"></span>
+        <span className="cloudFour"></span>
+        <span className="cloudFive"></span>
+        <span className="orangeClouds"></span>
+        <span className="orangeCloudsTwo"></span>
+        <span className="stars"></span>
+
+        <ReactAudioPlayer
+            src={BellsMusic}
+            autoPlay={true}
+            controls
+        />
     </div>
 );
  
